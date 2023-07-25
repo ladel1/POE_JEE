91d84cc7646fc5ef3a30faff34b15f4066406cb6
Adel LATIBI <alatibi@campus-eni.fr>
2023-07-25 14:41:20 +0200

    Les params d'init par annotation

--- demoInitParam/src/main/java/fr/eni/ihm/MainServlet.java
+++ demoInitParam/src/main/java/fr/eni/ihm/MainServlet.java
@@ -1,6 +1,7 @@
 package fr.eni.ihm;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,12 +9,22 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
+@WebServlet(
+			name="home",
+			urlPatterns = "/home",
+			initParams = {
+					@WebInitParam(
+								name="title",
+								value="Accueil"
+							)
+			}
+		)
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	public void init() throws ServletException {
-		System.out.println(this.getInitParameter("EMAIL_ADMIN"));
+		System.out.println(this.getInitParameter("title"));
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
