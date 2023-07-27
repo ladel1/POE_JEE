f6df613e2d226c9413cbf2991e919407e243a131
Adel LATIBI <alatibi@campus-eni.fr>
2023-07-27 09:56:37 +0200

    Démo gestion d'erreur sur JSP

---
+++ demoExceptionJee/src/main/java/fr/eni/ecole/ihm/MainServlet.java
@@ -0,0 +1,17 @@
+package fr.eni.ecole.ihm;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+	}
+
+}
---
+++ demoExceptionJee/src/main/webapp/WEB-INF/home.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" 
+    contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"
+    isErrorPage="false"
+    errorPage="/WEB-INF/errors/error.jsp"
+    %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Accueil</title>
+</head>
+<body>
+<h1>Accueil</h1>
+<%
+	String str = null;
+	str.length();
+%>
+</body>
+</html>
